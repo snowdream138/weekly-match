543e3b0c814257f57510d1bc44cf4c439f6cef13
Richard Li <licai@flux.utah.edu>
2018-05-27 21:53:22 -0600

    fix bugs

--- baby_db.py
+++
@@ -1,3 +0,0 @@
-baby_db = {"A":True, "B":False, "C":True, "D":False }
-# "name": "gender"
-bigdevil = "C"
--- generate-pairs.py
+++ generate-pairs.py
@@ -1,47 +1,66 @@
-#!/usr/bin/env python
-#
+#!/usr/bin/env python3
+
+import random
+from collections import deque
 from copy import copy
 from weekly_input import *
-from baby_db import *
+from gender_db import *
 
 
 def remove_devil_when_odd(babies):
-    if len(babies)%2:
+    if len(babies) % 2:
         babies.remove(bigdevil)
 
-def pick_one(baby, be_picked, avail):
-    baby_gender = baby_db[baby]
-    partner_gender = baby_gender ^ True
-    candidates = avail[int(partner_gender)] if avail[int(partner_gender)] else avail[int(not partner_gender)]
-    candidates = deque(candidates)
-    while be_picked:
+
+def pick_one(baby, to_pick, picked, avails, opposit):
+    a_gender = b_gender = gender_db[baby]
+    if opposit:
+        b_gender = a_gender ^ True
+    candidates = deque(avails[int(b_gender)])
+    while picked:
         baby_friend = candidates.popleft()
-        if baby_friend not in be_picked:
+        if baby_friend not in picked:
             candidates.append(baby_friend)
         else:
-            be_picked.remove(baby_friend)
+            picked.remove(baby_friend)
+            avails[int(b_gender)].remove(baby_friend)
             return baby_friend
     return ""
 
+
 def init(babies):
-    global baby_db
-    female = randome.shuffle(filter(lambda x: baby_db[x], babies))
-    male = randome.shuffle(filter(lambda x: not baby_db[x], babies))
-    return [male, female]
+    global gender_db
+    random.seed(random_seed)
+    female = list(filter(lambda x: gender_db[x], babies))
+    male = list(filter(lambda x: not gender_db[x], babies))
+    to_pick = deque(copy(babies[:len(babies) // 2]))
+    picked = copy(babies[len(babies) // 2:])
+    f_pick_m = min(len(set(female) & set(to_pick)),
+                   len(set(male) & set(picked)))
+    m_pick_f = min(len(set(female) & set(picked)),
+                   len(set(male) & set(to_pick)))
+    random.shuffle(female)
+    random.shuffle(male)
+    return [male, female], to_pick, picked, [m_pick_f, f_pick_m]
+
 
 def main():
-    global babies_sorted
+    global babies_sorted, gender_db, random_seed
     match_result = []
     babies = copy(babies_sorted)
     remove_devil_when_odd(babies)
-    available_list = init(babies)
-    to_pick = deque(copy(babies[:len(babies)/2+1]))
-    be_picked = copy(babies[len(babies)/2+1:])
+    avails, to_pick, picked, pick_opposit_cnt = init(babies)
     while to_pick:
         baby = to_pick.popleft()
-        baby_friend = pick_one(baby, be_picked)
+        oppo = bool(pick_opposit_cnt[int(gender_db[baby])])
+        baby_friend = pick_one(baby, to_pick, picked, avails, oppo)
         match_result.append((baby, baby_friend))
-    for pair in match_result:
-        print (pair)
+        if oppo:
+            pick_opposit_cnt[int(gender_db[baby])] -= 1
 
-    
+    for pair in match_result:
+        print(pair[0], ":", pair[1])
+
+
+if __name__ == '__main__':
+    main()
